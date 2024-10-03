df02a63b9f7d14a208a220d42c4e6820e2dad019
RoushanFW196 <roushankumar6338@gmail.com>
2024-10-03 23:41:50 +0530

    updated the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-
-import './App.css'
+import "./App.css";
+import Header from "./layouts/Header";
+import Body from "./components/body";
 
 function App() {
-
-
   return (
-    <>
-     <p>hello world</p>
-    </>
-  )
+    <div
+      style={{ backgroundColor: "lightgray", height: "auto", width: "100%" }}
+    >
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
-export default App
+export default App;
